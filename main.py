24f8512923d5fcd0a09b98fccb980e0a970908cf
ayushgupta4897 <ayushgupta4897@gmail.com>
2023-11-21 14:45:16 +0530

    Encapsulating all into classes

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import logging
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
-from utils.WebScraper import WebScraper
-from utils import gpt_client, prompts
-from config import OPEN_AI_GPT_4, OPEN_AI_GPT_35_TURBO, OPEN_AI_GPT_4_TURBO
+from utils.web_scraper import WebScraper
+from utils.gpt_client import GPTClient
+from utils.prompts import Prompts
+from config import OPEN_AI_GPT_4, OPEN_AI_GPT_4_TURBO
+from utils.error_messages import ErrorMessages
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+gpt_client = GPTClient()
 
 def is_valid_url(url):
     """Check if the URL is valid."""
@@ -28,29 +31,33 @@ async def check_compliance(request: Request):
 
     # Validate URLs
     if 'policy_url' not in data or 'website_url' not in data:
-        raise HTTPException(status_code=400, detail="Missing 'policy_url' or 'website_url' in the request")
+        raise HTTPException(status_code=400,
+                            detail=ErrorMessages.MISSING_URLS)
 
     if not is_valid_url(data['policy_url']):
-        raise HTTPException(status_code=400, detail="Invalid 'policy_url'")
+        raise HTTPException(status_code=400,
+                            detail=ErrorMessages.INVALID_POLICY_URL)
 
     if not is_valid_url(data['website_url']):
-        raise HTTPException(status_code=400, detail="Invalid 'website_url'")
+        raise HTTPException(status_code=400,
+                            detail=ErrorMessages.INVALID_WEBSITE_URL)
 
     try:
         policy_text = scrape_content(data['policy_url'])
         website_text = scrape_content(data['website_url'])
 
         if policy_text is None or website_text is None:
-            return JSONResponse({"error": "Error scraping content from one of the URLs"}, status_code=500)
+            logger.error(ErrorMessages.SCRAPE_ERROR)
+            return JSONResponse({"error": ErrorMessages.INTERNAL_SERVER_ERROR}, status_code=500)
 
-        formatted_prompts = prompts.getPromptForComplianceReport(policy_text, website_text)
-        compliance_report = gpt_client.GetResponseFromLLM(OPEN_AI_GPT_4, formatted_prompts)
+        formatted_prompts = Prompts.get_prompt_for_compliance_report(policy_text, website_text)
+        compliance_report = gpt_client.get_response_from_llm(OPEN_AI_GPT_4, formatted_prompts)
 
         return {"Report": compliance_report}
 
     except Exception as e:
-        # Generic error handling for any other unexpected issues
-        return JSONResponse({"error": str(e)}, status_code=500)
+        logger.error(f"Unexpected error occurred: {e}")
+        return JSONResponse({"error": ErrorMessages.INTERNAL_SERVER_ERROR}, status_code=500)
 
 @app.get('/health')
 def health_check():
--- utils/error_messages.py
+++ utils/error_messages.py
@@ -1,5 +1,7 @@
-MISSING_URLS = "Missing 'policy_url' or 'website_url' in the request"
-INVALID_POLICY_URL = "Invalid 'policy_url'"
-INVALID_WEBSITE_URL = "Invalid 'website_url'"
-SCRAPE_ERROR = "Error scraping content from one of the URLs"
-INTERNAL_SERVER_ERROR = "Internal Server Error"
+# error_messages.py
+class ErrorMessages:
+    MISSING_URLS = "Missing 'policy_url' or 'website_url' in the request."
+    INVALID_POLICY_URL = "Invalid 'policy_url'."
+    INVALID_WEBSITE_URL = "Invalid 'website_url'."
+    SCRAPE_ERROR = "Error scraping content from one of the URLs."
+    INTERNAL_SERVER_ERROR = "Internal server error. Please try again later."
